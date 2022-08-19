fd8c148b325ed902fd66665ee92581f0610ee283
emiruyan <emir-uyn@hotmail.com>
2022-08-19 16:23:42 +0300

    Ball Input Controller
    We started the movement of our ball in the BallInputControlelr Script.

--- ZigZallBall/Assets/GameFolders/Scripts/BallScripts/BallInputController.cs
+++ ZigZallBall/Assets/GameFolders/Scripts/BallScripts/BallInputController.cs
@@ -1,18 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BallInputController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public Vector3 _ballDirection; //Vector3 türünde bir değişken oluşturduk.
 
-    // Update is called once per frame
-    void Update()
+  private void Start()
+  {
+    _ballDirection = Vector3.left; //_ballDirection'un başlangıç yönü Left olacak. 
+  }
+ 
+  private void Update()
+  {
+    HandleBallInputs();
+  }
+
+  private void HandleBallInputs()//Ball'ın Inputlarını buradan alacağız.
+  {
+    if (Input.GetMouseButton(0)) //Ekrana her dokunduğumuzda 
     {
-        
+      ChangeBallDirection();
     }
+  }
+ 
+  private void ChangeBallDirection()//Ball'ın yönünü burada değiştiriyoruz.
+  {
+    if (_ballDirection.x == -1) //Ball default olarak lefte gidiyor. Eğer lefte gidiyor ise;
+    {
+      _ballDirection = Vector3.forward; //GetMouseButton(0) çağırdığımızda yönünü forward olarak değiştir.
+    }
+    else
+    {
+      _ballDirection = Vector3.left;//Tekrar GetMouseButton(0) çağırdığımızda yönünü tekrar left'e çekmiş oluyoruz.
+    }
+  }
 }

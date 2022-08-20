53d50d6f71a8418549a35b0645c59558868c466a
emiruyan <emir-uyn@hotmail.com>
2022-08-20 21:54:21 +0300

    Camera Follow Controller
    We have finished our CameraFollowController script. Camera now follows the Ball.

--- ZigZallBall/Assets/GameFolders/Scripts/Camera/CameraFollowController.cs
+++ ZigZallBall/Assets/GameFolders/Scripts/Camera/CameraFollowController.cs
@@ -1,13 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ZigZagBall.Ball;
 
-namespace ZigZagBall.Cameras
+namespace ZigZagBall.Camera
 {
-    
+    public class CameraFollowController : MonoBehaviour //Kameramızın Ball'ı takip etme işlemi bu classta gerçekleşecek.
+    {
+        [SerializeField] private Transform _ballTransform; //Ball Transformunu aldık
+
+        [SerializeField] [Range(0,3)] private float  _lerpValue;
+        
+        private Vector3 _offset; //Camera ve Ball arasındaki mesafeyi tutacak olan değişken.
+
+        private Vector3 _newPosition;
+
+        private void Start()
+        {
+            _offset = transform.position - _ballTransform.position; //Camera ve Ball arasındaki mesafeyi çıkardık ve offset değişkenine atadık.
+        }
+
+        private void LateUpdate()
+        {
+            SetCameraSmoothFollow();
+        }
+
+        private void SetCameraSmoothFollow()
+        {
+            _newPosition = Vector3.Lerp(transform.position, _ballTransform.position + _offset, _lerpValue * Time.deltaTime);
+            //Camera Ball'ı takip etsin ancak arasındaki mesafeyi(_offSet) korusun istiyoruz.
+            transform.position = _newPosition;
+        }
+    }
+
 }
 
-public class CameraFollowController : MonoBehaviour //Kameramızın Ball'ı takip etme işlemi bu classta gerçekleşecek.
-{
-
-}
